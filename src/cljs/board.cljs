c91ef007fa86c32a7257f3ec1bb819ec2b61a41c
Rafael Oliveira <ludug3r0@gmail.com>
2015-02-21 22:31:50 -0300

    Replacing concept of empty placement by empty-vertex

--- src/cljs/board.cljs
+++ src/cljs/board.cljs
@@ -1,40 +1,34 @@
 (ns board
   (:require [go.game :as game]))
 
-(defn- render-placement
-       [{:keys [color vertex line column]}]
-       [(keyword (clojure.string/join
-                   "."
-                   ["div"
-                    "stone"
-                    color
-                    (str "line-" line)
-                    (str "column-" column)]))])
+(defn- render-stone
+  [stone]
+  (let [vertex (second stone)
+        color (-> stone first name)
+        line (-> vertex first str)
+        column (-> vertex second str)]
+    [(keyword (clojure.string/join
+                "."
+                ["div"
+                 "stone"
+                 color
+                 (str "line-" line)
+                 (str "column-" column)]))]))
 
-(defn- render-ghost
-       [{:keys [color vertex line column]}]
-       [(keyword (clojure.string/join
-                   "."
-                   ["div"
-                    "stone"
-                    (str "ghost-" color)
-                    (str "line-" line)
-                    (str "column-" column)]))])
-
-(defn- placement [stone]
-      (let [vertex (second stone)]
-           {:color  (-> stone first name)
-            :vertex vertex
-            :line   (-> vertex first str)
-            :column (-> vertex second str)}))
-
+(defn- render-empty-vertex
+  [vertex]
+  [(keyword (clojure.string/join
+              "."
+              ["div"
+               "vertex"
+               (str "line-" (-> vertex first str))
+               (str "column-" (-> vertex second str))]))])
 
 (defn render
-      [game]
-      (let [configuration (game/configuration @game)
-            placements (map placement configuration)
-            empty-placements (map placement (game/empty-placements @game))]
-           [:div.board
-            (concat
-              (mapv render-placement placements)
-              (mapv render-ghost empty-placements))]))
+  [game]
+  (let [configuration (game/configuration @game)
+        empty-vertices (game/empty-vertices @game)]
+    [:div.board
+     (concat
+       (mapv render-stone configuration)
+       (mapv render-empty-vertex empty-vertices))]))
