45f40591514f55efe189ae9f68092460d460576c
Rafael Oliveira <ludug3r0@gmail.com>
2015-07-26 12:11:55 -0300

    created game ids are valid uuid4

--- test/cljs/go_client/handlers/game_test.cljs
+++ test/cljs/go_client/handlers/game_test.cljs
@@ -7,6 +7,8 @@
 
 (def starting-db db/default-db)
 
+(enable-console-print!)
+
 (deftest creating-a-new-game
          (let [starting-number-of-games (count (:games starting-db))
                expected-number-of-games (inc starting-number-of-games)
@@ -19,4 +21,12 @@
                changed-game-keys (set (keys (:games changed-db)))
                created-game-key (first (set/difference changed-game-keys starting-game-keys))]
            (is (= "New game title" (get-in changed-db [:games created-game-key :title])))))
+
+(deftest game-key-is-uuid4
+         (let [starting-game-keys (set (keys (:games starting-db)))
+               changed-db (game/create-game starting-db ["New game title"])
+               changed-game-keys (set (keys (:games changed-db)))
+               created-game-key (first (set/difference changed-game-keys starting-game-keys))
+               match (re-matches #"[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}" created-game-key)]
+           (is (not= nil match))))
 
