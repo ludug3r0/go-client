857d7615bcb659196ccd82cb41ca30ac062160b2
Rafael Oliveira <ludug3r0@gmail.com>
2015-07-26 11:37:42 -0300

    testing set title on game creation

--- test/cljs/go_client/handlers/game_test.cljs
+++ test/cljs/go_client/handlers/game_test.cljs
@@ -2,7 +2,8 @@
   (:require-macros [cemerick.cljs.test :refer [is deftest with-test run-tests testing test-var]])
   (:require [cemerick.cljs.test]
             [go-client.db :as db]
-            [go-client.handlers.game :as game]))
+            [go-client.handlers.game :as game]
+            [clojure.set :as set]))
 
 (def starting-db db/default-db)
 
@@ -11,4 +12,11 @@
                expected-number-of-games (inc starting-number-of-games)
                changed-db (game/create-game starting-db ["title"])]
            (is (= expected-number-of-games (count (:games changed-db))))))
+
+(deftest game-title
+         (let [starting-game-keys (set (keys (:games starting-db)))
+               changed-db (game/create-game starting-db ["New game title"])
+               changed-game-keys (set (keys (:games changed-db)))
+               created-game-key (first (set/difference changed-game-keys starting-game-keys))]
+           (is (= "New game title" (get-in changed-db [:games created-game-key :title])))))
 
